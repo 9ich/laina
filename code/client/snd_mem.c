12cceccfebcd70f458d6af6e8bf86a0b6ed663d8
ich <sunn>
2015-07-08 16:13:28 +0100

    snd: use cnq3 1.47 memory manager, which actually uses the hunk

--- code/client/snd_mem.c
+++ code/client/snd_mem.c
@@ -20,32 +20,19 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 ===========================================================================
 */
 
-/*****************************************************************************
- * name:		snd_mem.c
- *
- * desc:		sound caching
- *
- * $Archive: /MissionPack/code/client/snd_mem.c $
- *
- *****************************************************************************/
+/*#
+sound caching
+*/
 
 #include "snd_local.h"
 #include "snd_codec.h"
 
 #define DEF_COMSOUNDMEGS "8"
 
-/*
-===============================================================================
-
-memory management
-
-===============================================================================
-*/
-
-static	sndBuffer	*buffer = NULL;
-static	sndBuffer	*freelist = NULL;
-static	int inUse = 0;
-static	int totalInUse = 0;
+static sndBuffer* sndbuffers = NULL;
+static sndBuffer* freelist = NULL;
+static int sndmem_avail = 0;
+static int sndmem_inuse = 0;
 
 short *sfxScratchBuffer = NULL;
 sfx_t *sfxScratchPointer = NULL;
@@ -54,21 +41,18 @@ int	   sfxScratchIndex = 0;
 void	SND_free(sndBuffer *v) {
 	*(sndBuffer **)v = freelist;
 	freelist = (sndBuffer*)v;
-	inUse += sizeof(sndBuffer);
+	sndmem_avail += sizeof(sndBuffer);
 }
 
 sndBuffer*	SND_malloc(void) {
-	sndBuffer *v;
-redo:
-	if (freelist == NULL) {
+	while (!freelist) {
 		S_FreeOldestSound();
-		goto redo;
 	}
 
-	inUse -= sizeof(sndBuffer);
-	totalInUse += sizeof(sndBuffer);
+	sndmem_avail -= sizeof(sndBuffer);
+	sndmem_inuse += sizeof(sndBuffer);
 
-	v = freelist;
+	sndBuffer* v = freelist;
 	freelist = *(sndBuffer **)freelist;
 	v->next = NULL;
 	return v;
@@ -76,34 +60,26 @@ redo:
 
 void SND_setup(void) {
 	sndBuffer *p, *q;
-	cvar_t	*cv;
-	int scs;
 
-	cv = Cvar_Get( "com_soundMegs", DEF_COMSOUNDMEGS, CVAR_LATCH | CVAR_ARCHIVE );
+	// a sndBuffer is actually 2K+, so this isn't even REMOTELY close to actual megs
+	const cvar_t* cv = Cvar_Get( "com_soundMegs", DEF_COMSOUNDMEGS, CVAR_LATCH | CVAR_ARCHIVE );
+	int scs = cv->integer * 1024;
 
-	scs = (cv->integer*1536);
+	sndbuffers = (sndBuffer*)Hunk_Alloc( scs * sizeof(sndBuffer), h_high );
+	sndmem_avail = scs * sizeof(sndBuffer);
 
-	buffer = malloc(scs*sizeof(sndBuffer) );
-	// allocate the stack based hunk allocator
-	sfxScratchBuffer = malloc(SND_CHUNK_SIZE * sizeof(short) * 4);	//Hunk_Alloc(SND_CHUNK_SIZE * sizeof(short) * 4);
-	sfxScratchPointer = NULL;
-
-	inUse = scs*sizeof(sndBuffer);
-	p = buffer;;
+	p = sndbuffers;
 	q = p + scs;
 	while (--q > p)
 		*(sndBuffer **)q = q-1;
-	
+
 	*(sndBuffer **)q = NULL;
 	freelist = p + scs - 1;
-
-	Com_Printf("Sound memory manager started\n");
 }
 
 void SND_shutdown(void)
 {
 		free(sfxScratchBuffer);
-		free(buffer);
 }
 
 /*
@@ -213,7 +189,11 @@ qboolean S_LoadSound( sfx_t *sfx )
 	byte	*data;
 	short	*samples;
 	snd_info_t	info;
-//	int		size;
+
+	// player specific sounds are never directly loaded
+	if (sfx->soundName[0] == '*') {
+		return qfalse;
+	}
 
 	// load it in
 	data = S_CodecLoad(sfx->soundName, &info);
@@ -228,8 +208,6 @@ qboolean S_LoadSound( sfx_t *sfx )
 		Com_DPrintf(S_COLOR_YELLOW "WARNING: %s is not a 22kHz audio file\n", sfx->soundName);
 	}
 
-	samples = Hunk_AllocateTempMemory(info.channels * info.samples * sizeof(short) * 2);
-
 	sfx->lastTimeUsed = Com_Milliseconds()+1;
 
 	// each of these compression schemes works just fine
@@ -238,18 +216,23 @@ qboolean S_LoadSound( sfx_t *sfx )
 	// manager to do the right thing for us and page
 	// sound in as needed
 
+	samples = NULL;
+
 	if( info.channels == 1 && sfx->soundCompressed == qtrue) {
+		samples = Z_Malloc(info.channels * info.samples * sizeof(short) * 2);
 		sfx->soundCompressionMethod = 1;
 		sfx->soundData = NULL;
 		sfx->soundLength = ResampleSfxRaw( samples, info.channels, info.rate, info.width, info.samples, data + info.dataofs );
 		S_AdpcmEncodeSound(sfx, samples);
 #if 0
 	} else if (info.channels == 1 && info.samples>(SND_CHUNK_SIZE*16) && info.width >1) {
+		samples = Z_Malloc(info.channels * info.samples * sizeof(short) * 2);
 		sfx->soundCompressionMethod = 3;
 		sfx->soundData = NULL;
 		sfx->soundLength = ResampleSfxRaw( samples, info.channels, info.rate, info.width, info.samples, (data + info.dataofs) );
 		encodeMuLaw( sfx, samples);
 	} else if (info.channels == 1 && info.samples>(SND_CHUNK_SIZE*6400) && info.width >1) {
+		samples = Z_Malloc(info.channels * info.samples * sizeof(short) * 2);
 		sfx->soundCompressionMethod = 2;
 		sfx->soundData = NULL;
 		sfx->soundLength = ResampleSfxRaw( samples, info.channels, info.rate, info.width, info.samples, (data + info.dataofs) );
@@ -263,12 +246,13 @@ qboolean S_LoadSound( sfx_t *sfx )
 
 	sfx->soundChannels = info.channels;
 	
-	Hunk_FreeTempMemory(samples);
-	Hunk_FreeTempMemory(data);
+	if(samples != NULL)
+		Z_Free(samples);
+	Z_Free(data);
 
 	return qtrue;
 }
 
 void S_DisplayFreeMemory(void) {
-	Com_Printf("%d bytes free sound buffer memory, %d total used\n", inUse, totalInUse);
+	Com_Printf("%d bytes free sound buffer memory, %d total used\n", sndmem_inuse, sndmem_avail);
 }
