1cc73be9eacd5c3140460c4268e6ea4d5ce5c5d6
ich <sunn>
2015-09-29 13:55:47 -0700

    common: don't read qkey twice in a row

--- code/client/cl_main.c
+++ code/client/cl_main.c
@@ -1348,17 +1348,8 @@ update cl_guid using QKEY_FILE and optional prefix
 */
 static void CL_UpdateGUID( const char *prefix, int prefix_len )
 {
-	fileHandle_t f;
-	int len;
-
-	len = FS_SV_FOpenFileRead( QKEY_FILE, &f );
-	FS_FCloseFile( f );
-
-	if( len != QKEY_SIZE ) 
-		Cvar_Set( "cl_guid", "" );
-	else
-		Cvar_Set( "cl_guid", Com_MD5File( QKEY_FILE, QKEY_SIZE,
-			prefix, prefix_len ) );
+	Cvar_Set( "cl_guid", Com_MD5File( QKEY_FILE, QKEY_SIZE,
+		prefix, prefix_len ) );
 }
 
 static void CL_OldGame(void)
