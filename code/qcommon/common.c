b457408d08150e1fd13a0e9600c192698120d0bb
ich <sunn>
2015-09-30 16:21:44 +0100

    Revert "common: allow arbitrary com_maxfps"
    This reverts commit 04edbe4a89390cb87a049b01ec9bcd3029c55d17.

--- code/qcommon/common.c
+++ code/qcommon/common.c
@@ -3027,9 +3027,9 @@ Com_TimeVal
 =================
 */
 
-int Com_TimeVal(float minMsec)
+int Com_TimeVal(int minMsec)
 {
-	float timeVal;
+	int timeVal;
 
 	timeVal = Sys_Milliseconds() - com_frameTime;
 
@@ -3047,11 +3047,10 @@ Com_Frame
 =================
 */
 void Com_Frame( void ) {
-	int 		msec;
-	float		minMsec;
+
+	int		msec, minMsec;
 	int		timeVal, timeValSV;
-	static int	lastTime = 0;
-	static float	bias = 0;
+	static int	lastTime = 0, bias = 0;
  
 	int		timeBeforeFirstEvents;
 	int		timeBeforeServer;
@@ -3088,13 +3087,13 @@ void Com_Frame( void ) {
 		else
 		{
 			if(com_minimized->integer && com_maxfpsMinimized->integer > 0)
-				minMsec = 1000.0f / com_maxfpsMinimized->integer;
+				minMsec = 1000 / com_maxfpsMinimized->integer;
 			else if(com_unfocused->integer && com_maxfpsUnfocused->integer > 0)
-				minMsec = 1000.0f / com_maxfpsUnfocused->integer;
+				minMsec = 1000 / com_maxfpsUnfocused->integer;
 			else if(com_maxfps->integer > 0)
-				minMsec = 1000.0f / com_maxfps->integer;
+				minMsec = 1000 / com_maxfps->integer;
 			else
-				minMsec = 0.0f;
+				minMsec = 1;
 			
 			timeVal = com_frameTime - lastTime;
 			bias += timeVal - minMsec;
@@ -3108,7 +3107,7 @@ void Com_Frame( void ) {
 		}
 	}
 	else
-		minMsec = 0.0f;
+		minMsec = 1;
 
 	do
 	{
